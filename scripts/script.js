76650ad34b7bc37e90ec2439029106b40a8e7bdb
swardak <shkula.wardak@gmail.com>
2023-09-04 18:01:54 -0400

    2023 update. one day i shall fix the mess that is styles.css

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,9 @@
 /*the following was borrowed & tweaked frm w3schools.com :)*/
 function toggleHamburger() {
-    var x = document.getElementById("nav");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+  var x = document.getElementById("nav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
   }
-
-  var navbar = document.getElementById("nav");
-  var navItem = navbar.getElementsByClassName("nav-item");
-  for (var i = 0; i < navItem.length; i++) {
-    navItem[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace("active", "");
-      this.className += " active";
-    });
-  }
+}
